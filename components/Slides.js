c242809308ebfd2dfff114c9dc5f28d4d2cf5c59
Daniel Velez <velez.a.daniel@gmail.com>
2017-09-30 01:50:46 -0400

    still need to work on welcome button

--- components/Slides.js
+++ components/Slides.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import { View, Text, ScrollView, Dimensions } from 'react-native';
+import { Button } from 'react-native-elements';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SCREEN_HEIGHT = Dimensions.get('window').height;
 export default class Slides extends React.Component {
+  renderLastSlide(index) {
+    if (index === this.props.data.lenght - 1) {
+      return (
+        <Button
+          title="ready?"
+          raised
+          buttonStyle={styles.buttonStyle}
+        />
+      );
+    }
+  }
+
   renderSlides() {
-    return this.props.data.map(slide => {
+    return this.props.data.map((slide, index) => {
       return (
         <View
           key={slide.text}
           style={[styles.slideStyle, { backgroundColor: slide.color }]}
         >
           <Text style={styles.textStyle}>{slide.text}</Text>
-
+          {this.renderLastSlide(index)}
         </View>
       );
     });
