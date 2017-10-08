2100f52c2ced1fdc777ee27e27aabcfaa5be4b3d
Daniel Velez <velez.a.daniel@gmail.com>
2017-10-07 23:10:53 -0400

    still working on getting push notifications to work

--- reducers/likes_reducer.js
+++ reducers/likes_reducer.js
@@ -1,10 +1,14 @@
 import _ from 'lodash';
+import { REHYDRATE } from 'redux-persist/constants';
 import {
   LIKE_JOB, CLEAR_LIKED_JOBS
 } from '../actions/types';
 
+// use lodash whenever you want to parse an object {}
 export default function (state = [], action) {
   switch (action.type) {
+    case REHYDRATE:
+      return action.payload.likes || [];
     case LIKE_JOB:
       return _.uniqBy([action.payload, ...state], 'jobkey');
     case CLEAR_LIKED_JOBS:
--- store/index.js
+++ store/index.js
@@ -1,13 +1,19 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { persistStore, autoRehydrate } from 'redux-persist';
+import { AsyncStorage } from 'react-native';
 import reducers from '../reducers';
 
 const store = createStore(
   reducers,
   {},
   compose(
-    applyMiddleware(thunk)
+    applyMiddleware(thunk),
+    autoRehydrate()
   )
 );
+// purge() will give the option to reset persistStore state
+persistStore(store, { storage: AsyncStorage, whitelist: ['likes'] }); //.purge()
+
 
 export default store;
